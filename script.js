a679c7f3f6c12632811013026852bf7793a4f41f
lily1215z <odessa1215@i.ua>
2021-11-27 01:01:53 +0200

    add tasl

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function uniCode (str) {
     return a;
   }
   uniCode('aaa');
+
+
+  //код не работает. Нужно доделать
+  // function correct(string) {
+  //   for(let i=0; i < string.length; i++) {
+  //     if(string[i] == 5) {
+  //       return string = string.replace(/5/g, 's').toUpperCase();
+  //     } else if (string[i] == 0) {
+  //       console.log(string);
+  //       return string = string.replace(/0/g, 'o').toUpperCase();
+  //     }
+  //   }
+
+  // }
+  // correct('L0ND015N');
+ 
