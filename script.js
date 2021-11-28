85cc9c6d65993319941fe0caa00f323b8d500fca
lily1215z <odessa1215@i.ua>
2021-11-28 22:17:50 +0200

    add tasks

--- script.js
+++ script.js
@@ -6,23 +6,114 @@ function uniCode (str) {
     for(let i = 0; i < str.length; i++) {
       a += str[i].charCodeAt();
     }
-    console.log(a);
     return a;
   }
   uniCode('aaa');
 
 
-  //код не работает. Нужно доделать
-  // function correct(string) {
-  //   for(let i=0; i < string.length; i++) {
-  //     if(string[i] == 5) {
-  //       return string = string.replace(/5/g, 's').toUpperCase();
-  //     } else if (string[i] == 0) {
-  //       console.log(string);
-  //       return string = string.replace(/0/g, 'o').toUpperCase();
-  //     }
-  //   }
+//строка должна менять число на букву.
+  function correct(string) {
+    let a = string.split(''); //разбила на массив
+    for(let i=0; i < a.length; i++) {
+      if(a[i] === '5') {
+        a[i] = 's'.toUpperCase();
+      } else if(a[i] === '0') {
+        a[i] = 'o'.toUpperCase();
+      } else if(a[i] === '1') {
+        a[i] = 'i'.toUpperCase();
+      } else if(a[i] === '') {
+        a[i] = '';
+      }
+    }
+    return a.join('');  //собрала в строку
+  }
+  correct('L0ND0N 55orry');
 
-  // }
-  // correct('L0ND015N');
- 
+  //2 вариант. Меняем 1 на I, 5 = S, 0 = O;
+function correct1(string) {
+  return string.replace(/1/g, 'I').replace(/5/g, 'S').replace(/0/g, 'O');
+}
+correct1('L0nD0N 5orry');
+
+// при вводе числа выводится это число словом. Чтоб не испол switch сase можно через массив
+  function switchItUp(number){
+    const words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+    return words[number];
+  }
+
+  switchItUp(2);
+  console.log(switchItUp(3));
+
+  // или эту же задачу можно решить через обьекты
+  function switchItUp(number){
+    var a = number;
+    var b = {
+      0: "Zero",
+      1: "One",
+      2: "Two",
+      3: "Three",
+      4: "Four",
+      5: "Five",
+      6: "Six",
+      7: "Seven",
+      8: "Eight",
+      9: "Nine"
+    };
+    return b[a];
+  }
+
+
+  function rentalCarCost(d) {
+    const oneDay = 40;
+    let res;
+    if(d >= 1) {
+      res = d * oneDay;
+    } if(d >= 3) {
+      res = d * oneDay - 20;
+    } if(d >= 7) {
+      res = d * oneDay - 50;
+    }
+    return res;
+
+  }
+  rentalCarCost(8);
+
+  //определяем индекс массы человека
+  function bmi(weight, height) {
+    const bmi = weight / (height * height);  //or height**2;
+    if(bmi <= 18.5) {
+      return 'Underweight';
+    } if(bmi <= 25) {
+      return 'Normal';
+    } if(bmi <= 30) {
+      return 'Overweight'
+    } if(bmi > 30) {
+      return 'Obese';
+    }
+    return;
+  }
+  bmi(80, 1.8);
+
+  //2 способ через тернарный оператор
+  function bmi1(weight, height) {
+  const bmi  = weight/(height*height);
+  console.log(bmi);
+  return bmi < 18.5 ? "Underweight" : bmi <=25 ? "Normal" : bmi <= 30 ? "Overweight" : "Obese";
+  }
+  bmi1(80, 1.8);
+
+  //3 способо через свит кейс
+  function bmi2(weight, height) {
+    let bmi = weight / (height * height);
+    switch(true){
+      case bmi <= 18.5:
+        return "Underweight";
+      case bmi <= 25.0:
+        return "Normal";
+      case bmi <= 30.0:
+        return "Overweight";
+      case bmi > 30:
+        return "Obese";
+     }
+   }
+   bmi2(80, 1.8);
